aad210db541cafc3ed0219ccd9ec9b5c52c66752
kevinnammour <nammourkevin@gmail.com>
2022-03-27 13:55:57 +0300

    post signup credentials

---
+++ server/apis/auth/sign-up.php
@@ -0,0 +1,8 @@
+<?php
+    $full_name = $_POST['full_name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    echo $username . ' ' . $password;
+?>

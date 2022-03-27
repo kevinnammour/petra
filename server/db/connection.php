62a2b3038d8b14e99a0dcb85095140ee50b3163b
kevinnammour <nammourkevin@gmail.com>
2022-03-27 12:11:54 +0300

    db connection

--- server/db/connection.php
+++ server/db/connection.php
@@ -1,3 +1,19 @@
 <?php
     require "../vendor/autoload.php";
+
+    // loads the .env file
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . './../');
+    // suppresses any exception that is thrown if the .env does not exist
+    $dotenv->safeLoad();
+    
+    $db_host = $_ENV['DB_HOST'];
+    $db_user = $_ENV['DB_USER'];
+    $db_pass = $_ENV['DB_PASS'];
+    $db_name = $_ENV['DB_NAME'];
+
+    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    if(mysqli_connect_errno()) {
+        die("Failed connection");
+    }
 ?>

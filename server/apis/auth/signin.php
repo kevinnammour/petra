e08a9184dc746dc0a2ded612cf1e06c177f94705
kevinnammour <nammourkevin@gmail.com>
2022-03-28 13:47:16 +0300

    response

--- server/apis/auth/signin.php
+++ server/apis/auth/signin.php
@@ -1,12 +1,13 @@
 <?php
-    require(__DIR__.'/../../db/connection.php');
+    require __DIR__ . '/../../db/config.php';
 
     $email_or_username = $_POST['email_or_username'];
     $password = $_POST['password'];
 
     $query;
     $regex = '/\@/';
-    if(preg_match($regex, $email_or_username) === 1) {
+    if (preg_match($regex, $email_or_username) === 1) {
+        // If it includes a '@' character, it is an email
         $query = $mysqli->prepare("SELECT * FROM users WHERE email = ?");
     } else {
         // Since username cannot contain an @ (prohibited on sign up)
@@ -18,23 +19,24 @@
     $array = $query->get_result();
 
     $response = [];
-    
+
     while ($user = $array->fetch_assoc()) {
         $response[] = $user;
     }
 
-    // $options = array('cost'=>11);
     $hash = $response[0]['password'];
     $json_response;
 
-    if(password_verify($password, $hash)) {
-        $json_response = array( 
-            "user_id" => $response[0]['user_id']
-        );
+    if (password_verify($password, $hash)) {
+        $json_reponse = json_encode(array(
+            'success' => true,
+            'result' => $response[0],
+        ));
     } else {
-        $json_response = array( 
-            "status" => 401
-        );
+        $json_reponse = json_encode(array(
+            'success' => false,
+            'message' => 'Invalid credentials',
+        ));
     }
     echo json_encode($json_response);
 ?>

5b93de16e13e8b3cde12321fb3d63b37b14a83f0
kevinnammour <nammourkevin@gmail.com>
2022-04-15 19:46:03 +0300

    country and county

--- server/apis/account/add_activity.inc.php
+++ server/apis/account/add_activity.inc.php
@@ -1,5 +1,6 @@
 <?php
     require __DIR__ . '/../../db/config.inc.php';
+    require(__DIR__ . '/../../vendor/autoload.php');
 
     $user_id = $_POST['user_id'];
     $category = $_POST['category'];
@@ -9,8 +10,19 @@
     $longitude = $_POST['longitude'];
     $activity_image = $_POST['activity_image'];
 
-    $query = $mysqli->prepare("INSERT INTO activities (user_id, category, description, price, latitude, longitude, activity_image) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $query->bind_param("issdddb", $user_id, $category, $description, $price, $latitude, $longitude, $activity_image);
+    // Getting a readable address for that activity based on the lat and long
+    // More specifically, we are getting the country, and county
+    $ACCESS_KEY = $_ENV['ACCESS_KEY'];
+    $loc_api = "http://api.positionstack.com/v1/reverse?access_key=$ACCESS_KEY&query=$latitude,$longitude";
+    $loc_api_res = file_get_contents($loc_api);
+    $std_obj = json_decode($loc_api_res);
+    $address = json_decode(json_encode($std_obj), true);
+    $country = $address['data'][0]['country'];
+    $county = $address['data'][0]['county'];
+
+
+    $query = $mysqli->prepare("INSERT INTO activities (user_id, category, description, price, latitude, longitude, country, county, activity_image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $query->bind_param("issdddssb", $user_id, $category, $description, $price, $latitude, $longitude, $country, $county, $activity_image);
 
     $response;
     try {
---
+++ server/apis/home/search.inc.php
@@ -0,0 +1,11 @@
+<?php
+    require __DIR__ . '/../../db/config.inc.php';
+
+    $min = $_GET['min'];
+    $max = $_GET['max'];
+    $sunny = $_GET['sunny'];
+    $rainy = $_GET['rainy'];
+    $cloudy = $_GET['cloudy'];
+    $snowy = $_GET['snowy'];
+    
+?>

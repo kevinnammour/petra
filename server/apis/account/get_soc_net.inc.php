ad8a511b9e4982c753e5241870bfd078e4a5ee64
kevinnammour <nammourkevin@gmail.com>
2022-04-15 15:42:08 +0300

    get soc net api

---
+++ server/apis/account/get_soc_net.inc.php
@@ -0,0 +1,22 @@
+<?php
+    require __DIR__ . '/../../db/config.inc.php';
+
+    $user_id = $_GET['user_id'];
+    $query = $mysqli->prepare('SELECT whatsapp, instagram, facebook, tiktok, user_image FROM users WHERE user_id = ?');
+    $query->bind_param('i', $user_id);
+
+    $response;
+
+    try {
+        $query->execute();
+        $result = $query->get_result();
+        while ($user = $result->fetch_assoc()) {
+            $response = $user;
+        }
+        $response['status'] = 200;
+    } catch (mysqli_sql_exception $e) {
+        // Unknown error
+        $response = array('status' => 500);
+    }
+    echo json_encode($response);
+?>

768a81151fb877b4b07ebffab50f8981e27f1225
kevinnammour <nammourkevin@gmail.com>
2022-04-18 23:47:23 +0300

    signup frontend

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   },
+  {
+    path: 'landing',
+    loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)
+  },
 ];
 
 @NgModule({

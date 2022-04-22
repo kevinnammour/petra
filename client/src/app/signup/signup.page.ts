772e2f9ec12f8dc454b93d1a2e3ef547cefc485b
kevinnammour <nammourkevin@gmail.com>
2022-04-22 20:09:59 +0300

    register form validation

--- client/src/app/signup/signup.page.ts
+++ client/src/app/signup/signup.page.ts
@@ -1,5 +1,7 @@
+/* eslint-disable max-len */
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,8 +9,37 @@ import {Router} from '@angular/router';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  registerForm = new FormGroup({
+    fullName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20),
+      Validators.pattern(/^[A-Za-z]+$/),
+    ]),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=[a-zA-Z0-9._]{6,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      ),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    ])
+  });
 
-  constructor(private route: Router) { }
+  // Password rules:
+  // At least one upper case English letter, (?=.*?[A-Z])
+  // At least one lower case English letter, (?=.*?[a-z])
+  // At least one digit, (?=.*?[0-9])
+  // At least one special character, (?=.*?[#?!@$%^&*-])
+  // Minimum eight in length .{8,} (with the anchors)
+
+  constructor(private route: Router) {}
 
   goToLandingPage() {
     this.route.navigate(['landing']);
@@ -18,7 +49,5 @@ export class SignupPage implements OnInit {
     this.route.navigate(['login']);
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- server/apis/auth/cookies.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $token = openssl_random_pseudo_bytes(16);
- 
-    //Convert the binary data into hexadecimal representation.
-    $token = bin2hex($token);
-     
-    //Print it out for example purposes.
-    echo $token;
-?>
